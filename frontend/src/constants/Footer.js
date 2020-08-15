5796c9da40fb6e5cd842e14960fb622e0c90b2af
rachel123nkr <44622520+rachel123nkr@users.noreply.github.com>
2020-08-16 00:06:38 +0300

    commit message

---
+++ frontend/src/constants/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+class Footer extends React.Component {
+    render() {
+        return (
+
+            <div className="footer">
+                <div className="container">
+
+                    <div className="agileits-w3layouts-copyright">
+                        <p>© rachel & liat Royal Recipes </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ frontend/src/screens/AbuatScreen.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+function About() {
+    return (
+
+        <div className="container">
+            <div className="contact-form">
+                <h3>About</h3>
+                <div>
+                <div className="w3l-about-top">
+                    <div className="container">
+                        <div className="w3ls-about-info">
+                            <img src="images/a.jpg" alt=""/>
+                            <p>Praesent dignissim, nisi vel tristique convallis, urna nunc iaculis nunc, eu volutpat massa
+                                quam ac dolor. Proin lacinia in nibh eget maximus. Suspendisse vitae mi porttitor, varius
+                                nulla a, eleifend nisl. Etiam porta ut mauris a aliquam. Cras pellentesque elementum eros,
+                                eget tristique velit malesuada in. Phasellus diam ligula, molestie sed porta et, aliquam ut
+                                ante. Vivamus gravida nec risus et euismod. Maecenas pharetra arcu in vehicula ornare. Duis
+                                faucibus vehicula purus. Quisque vel neque vestibulum, tempus purus quis, porta erat. Aliquam
+                                nec pharetra sapien. Etiam gravida porta tortor, sit amet maximus purus tempus non. Integer
+                                egestas lobortis interdum. Curabitur sit amet eleifend velit, quis ultrices quam. Nullam
+                                tincidunt justo id magna condimentum, eu rhoncus eros tincidunt. Aenean ultrices dui ac
+                                blandit finibus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="jarallax different">
+                    <div className="container">
+                        <div className="w3-different-heading">
+                            <h3>Why we are different</h3>
+                        </div>
+                        <div className="w3agile-different-info">
+                            <p>Cras vehicula massa id ipsum venenatis, non convallis libero pellentesque. Sed tristique massa
+                                et mattis mattis. Nunc euismod consequat ex, eu tincidunt justo malesuada vitae. Sed auctor
+                                diam quis nulla hendrerit porta. Quisque venenatis, tortor quis tristique congue, lorem lectus
+                                elementum augue, in laoreet lorem justo lacinia velit. Phasellus tempus luctus velit, eu
+                                fermentum arcu porttitor sit amet. Suspendisse id efficitur sem. Donec laoreet ante at
+                                facilisis dictum. Donec sodales porta orci sed porta. Vivamus est enim, pharetra sit amet
+                                placerat ut, eleifend quis purus. Sed elementum tortor erat, placerat rutrum augue facilisis
+                                vel. Morbi feugiat tortor erat, nec gravida mauris egestas tincidunt. Sed vitae dignissim
+                                sapien.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="jarallax different">
+                    <div className="container">
+                        <div className="w3-different-heading">
+                            <h3>Our Branchs</h3>
+                        </div>
+                        <div className="w3agile-different-info">
+                            <p>Haifa * Jerusalem * Tel-Aviv</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+export default About;
